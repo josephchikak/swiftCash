704bb974878ab492ed578549446a33d0786cd4b0
raytheboffin <111377374+josephchikak@users.noreply.github.com>
2023-09-01 14:32:53 +0300

    added new media queries

--- src/Components/POS.jsx
+++ src/Components/POS.jsx
@@ -5,12 +5,12 @@ const POS = () => {
 
   const landingSmall1 =decodeBlurHash('LND,l,WC.TouTejZs:W.xFkBWWah', 32, 32);
   return (
-    <div id='POS' className='h-[70vh] md:h-[60vh] lg:h-[70vh] w-screen flex flex-col sm:flex-row '>
-        <div className='w-[100vw] sm:w-[50vw] h-[70%] sm:h-[100%] flex gap-10 justify-center items-center sm:pl-[10%] relative'>
+    <div id='POS' className='h-[100vh] sm:h-[70vh] md:h-[60vh] lg:h-[70vh] w-screen flex flex-col sm:flex-row '>
+        <div className='w-[100vw] sm:w-[50vw] h-[70%] sm:h-[100%] flex gap-10 justify-center items-center sm:pl-[10%] relative mb-10'>
           <div className='flex justify-end flex-col absolute w-[100%]  lg:w-[75%] h-[100%] lg:pl-0 md:pl-[0] pl-[10%] pb-[10%]'>
              <div className='h-[50%]'></div>
 
-              <div className='h-[50%] w-[70%] md:w-[85%] md:pr-[10%] lg:pr-0 ml-[10%] lg:ml-[0] lg:w-[100%] -mb-5 lg:-mb-5 z-0'>
+              <div className='h-[50%] w-[70%] md:w-[85%] md:pr-[10%] lg:pr-0 ml-[10%] lg:ml-[0] lg:w-[100%] mb-5 lg:-mb-5 z-0'>
                   <svg width="" height="" viewBox="0 0 594 318" fill="none" xmlns="http://www.w3.org/2000/svg">
                   <path fillRule="evenodd" clipRule="evenodd" d="M29.8498 123.63C55.9306 84.5152 120.19 75.6152 173.384 49.3957C214.095 29.3289 256.461 58.701 306.097 49.3957C368.97 37.609 432.938 -18.0736 486.18 5.9534C546.725 33.2762 584.762 77.0855 591.348 123.63C598.526 174.361 595.765 241.772 522.716 264.181C448.465 286.959 380.817 315.263 298.222 317.687C205.735 320.4 133.287 305.354 51.6539 278.001C-23.9717 252.662 -3.19374 173.187 29.8498 123.63Z" fill="#AAF015"/>
                   </svg>
@@ -21,10 +21,10 @@ const POS = () => {
          
             <img loading='lazy' className='h-[45%] lg:h-[70%]  top-[30%] lg:top-[25%] right-[5%] lg:right-[25%] z-10 ' src="/Assets for Swiftpocket/mockups/landing3.png" alt="" />
             
-            <img loading='lazy' className='h-[45%] lg:h-[70%]   top-[30%] lg:top-[25%] left-[30%] md:left-[35%] lg:left-[20%] z-10' src="/Assets for Swiftpocket/mockups/landing4.png" alt="" />
+            <img loading='lazy' className='h-[45%] lg:h-[70%]  top-[30%] lg:top-[25%] left-[30%] md:left-[35%] lg:left-[20%] z-10' src="/Assets for Swiftpocket/mockups/landing4.png" alt="" />
         </div>
 
-        <div className='sm:w-[50%] sm:h-[50%] h-[20%] pl-10 sm:pl-20 flex justify-start items-center mt-[10%]'>
+        <div className='sm:w-[50%] sm:h-[50%] h-[20%] pl-10 sm:pl-20 flex justify-start items-center mb-[20%]'>
             <div className='flex flex-col items-start justify-center md:w-[100%] lg:w-[70%] w-[68%] '>
         
                 <h1 className='font-bold md:text-[20px] lg:text-[30px] font-interHead sm:leading-[50px] '>Make POS payments with your Smart Phone</h1>
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -15,15 +15,15 @@ const Home = () => {
         <Nav/>
         <div id='hero' className='flex flex-col sm:flex-row sm:pl-[15%] pl-[10%] sm:pr-[5%] pr-[10%] h-screen w-[100vw]'>
             <div className='sm:w-[50vw] h-[90%] sm:h-[100%] flex justify-start items-center'>
-                <div className='sm:w-[554px] sm:h-[115px] flex flex-col gap-[20px]'>
+                <div className='sm:w-[554px] sm:h-[115px] w-[100%] flex flex-col gap-[20px]'>
                   <h1 className='font-bold text-[40px] font-interHead'>Elevate Your Payment Experience With <span className='text-[color:var(--primary-color)]'>Swift</span><span className='text-[#33db0a]' >Cash</span> </h1>
                   <p className="text-wrapper text-[95%]">
                      Save all your cards in a secured virtual wallet and enjoy contactless payment with just your phone.
                   </p>
                   <div className='flex gap-[30px] h-fit  text-white'>
-                    <button id='playstore' className='flex justify-center items-center gap-[10px] bg-[color:var(--alternate-primary-color)] p-[10px] sm:p-[12px] sm:w-[200px] h-fit w-full rounded-[10px]'>
-                        <span className='sm:w-[40px] sm:h-[40px]'>
-                            <svg width="30" height="30" viewBox="0 0 34 34" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <button id='playstore' className='flex justify-center items-center gap-[10px] bg-[color:var(--alternate-primary-color)] p-[10px] sm:p-[12px] sm:w-[200px] h-fit w-fit rounded-[10px]'>
+                        <span className='w-[20px] h-[20px] sm:w-[40px] sm:h-[40px]'>
+                            <svg width="" height="" viewBox="0 0 34 34" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path d="M17.7279 17.0352L6.20898 28.2502C6.57732 29.5004 7.75315 30.4567 9.15069 30.4567C9.73861 30.4567 10.2536 30.3093 10.6949 30.0154L23.5653 22.7344L17.7279 17.0352Z" fill="#E6E6E6" stroke="#82B840" stroke-linecap="round" stroke-linejoin="round"/>
                             <path d="M29.0061 14.3541L23.4911 11.1921L17.7274 17.0387L23.564 22.738L29.0805 19.6496C30.0368 19.1347 30.6984 18.1055 30.6984 17.0019C30.6247 15.8983 29.9624 14.8691 29.0061 14.3541ZM6.20843 5.75C6.13477 5.97029 6.13477 6.26496 6.13477 6.55892V27.5185C6.13477 27.8125 6.13477 28.0335 6.20843 28.3274L17.7274 17.0387L6.20843 5.75Z" fill="#E6E6E6"/>
                             <path d="M17.7274 17.0387L23.4911 11.1921L29.0061 14.3541C29.9624 14.8691 30.6247 15.8983 30.6984 17.0019C30.6984 18.1055 30.0368 19.1347 29.0805 19.6496L23.564 22.738L17.7274 17.0387ZM17.7274 17.0387L6.20843 28.3274C6.13477 28.0335 6.13477 27.8125 6.13477 27.5185V6.55892C6.13477 6.26496 6.13477 5.97029 6.20843 5.75L17.7274 17.0387Z" stroke="#82B840" stroke-linecap="round" stroke-linejoin="round"/>
@@ -32,18 +32,18 @@ const Home = () => {
 
                         </span>
 
-                        <p className='sm:w-[80%] text-left sm:text-[100%] text-[80%]'>Download on <span className='font-bold block sm:text-[22px] font-interHead'>Play Store</span></p>
+                        <p className=' sm:w-[80%] text-left sm:text-[100%] text-[80%] h-fit'>Download on <span className='font-bold block sm:text-[22px] font-interHead'>Play Store</span></p>
                     </button>
 
-                    <button id='appstore' className='flex justify-center items-center gap-[10px] bg-[color:var(--secondary-color)] p-[10px] sm:p-[12px] sm:w-[200px] h-fit w-full rounded-[10px]'>
-                        <span className='sm:w-[40px] sm:h-[40px]'>
-                            <svg width="30" height="30" viewBox="0 0 34 34" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <button id='appstore' className='flex justify-center items-center gap-[10px] bg-[color:var(--secondary-color)] p-[10px] sm:p-[12px] sm:w-[200px] h-fit  w-fit rounded-[10px]'>
+                        <span className=' w-[20px] h-[20px] sm:w-[40px] sm:h-[40px]'>
+                            <svg width="" height="" viewBox="0 0 34 34" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path d="M24.1545 28.73C22.7661 30.0758 21.2503 29.8633 19.7911 29.2258C18.247 28.5742 16.8303 28.5458 15.2011 29.2258C13.1611 30.1042 12.0845 29.8492 10.8661 28.73C3.95281 21.6042 4.97281 10.7525 12.8211 10.3558C14.7336 10.455 16.0653 11.4042 17.1845 11.4892C18.8561 11.1492 20.457 10.1717 22.242 10.2992C24.3811 10.4692 25.9961 11.3192 27.0586 12.8492C22.6386 15.4983 23.687 21.3208 27.7386 22.95C26.9311 25.075 25.8828 27.1858 24.1403 28.7442L24.1545 28.73ZM17.0428 10.2708C16.8303 7.11167 19.3945 4.505 22.3411 4.25C22.752 7.905 19.0261 10.625 17.0428 10.2708Z" fill="#E6E6E6"/>
                             </svg>
 
                         </span>
 
-                        <p className='sm:w-[80%] text-left sm:text-[100%] text-[80%]'>Download on <span className='font-bold sm:text-[22px] font-interHead block'>App Store</span></p>
+                        <p className='sm:w-[80%] text-left sm:text-[100%] text-[80%] '>Download on <span className='font-bold sm:text-[22px] font-interHead block'>App Store</span></p>
                     </button>
 
                   </div>
